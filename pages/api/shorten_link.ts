4679327a551cf29d18d802fa10e83e152342afad
Mark Holtzhausen <nemesarial@gmail.com>
2021-04-26 17:43:17 +0200

    test development branch

--- pages/api/shorten_link.ts
+++ pages/api/shorten_link.ts
@@ -4,7 +4,8 @@ export default async (req, res) => {
   const db = await connectToDatabase();
 
   if (req.body !== '' && req.body.link !== undefined && req.body.link !== '') {
-    const entry = await db.db('links_db').collection('links_collection').insertOne({ link: req.body.link });
+    const collection = db.db('shorty').collection('links_collection')
+    const entry = await collection.insertOne({ link: req.body.link });
 
     res.statusCode = 201;
     return res.json({ short_link: `https://${process.env.VERCEL_URL}/r/${entry.insertedId}` });
